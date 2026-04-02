4f8b4baff8b8b10a66938118e5bfe8844f3ef2fd
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-04-02 18:40:18 +0000

    File changes

--- src/components/menu/PromoBanners.jsx
+++ src/components/menu/PromoBanners.jsx
@@ -94,7 +94,7 @@ export default function PromoBanners({ onCategorySelect }) {
               src={banner.image}
               alt={banner.alt}
               className="w-full object-cover"
-              style={{ display: "block", height: 130, objectPosition: "center" }}
+              style={{ display: "block", height: 160, objectPosition: "center" }}
             />
           </motion.div>
         </AnimatePresence>
