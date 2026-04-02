1e6d1f0a36e250e71cf4145b754306608616b930
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-04-02 18:42:56 +0000

    File changes

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -207,7 +207,7 @@ export default function Menu() {
       </div>
 
       {/* ── CONTENIDO SCROLLABLE ── */}
-      <div className="pb-36 -mt-2">
+      <div className="pb-6 -mt-2">
 
         {/* ── BANNER CONTEXTUAL POR HORA ── */}
         <button
