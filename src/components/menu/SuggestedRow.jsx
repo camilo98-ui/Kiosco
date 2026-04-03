06b6bfd18184d726b464028ca700acd23baf5938
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-04-03 00:29:36 +0000

    Visual edits

--- src/components/menu/SuggestedRow.jsx
+++ src/components/menu/SuggestedRow.jsx
@@ -8,17 +8,12 @@ const PASTELS = ["#FFF0F5", "#F0FFF4", "#FFFAF0", "#FFF5F0", "#F0F5FF"];
 export default function SuggestedRow({ products, onAdd }) {
   if (!products || products.length === 0) return null;
 
-  return (
-    <div className="mb-6" style={{ paddingBottom: 80 }}>
-      {/* Label sección */}
-      
+  return null;
 
 
 
 
-      
 
-      
 
 
 
@@ -106,7 +101,12 @@ export default function SuggestedRow({ products, onAdd }) {
 
 
 
-      
-    </div>);
+
+
+
+
+
+
+
 
 }
