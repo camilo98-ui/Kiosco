396e93af5d979d218aa67d1d6dbb0eb536f21055
Cristian Camilo Rivera.Cuellar <cristian.rivera@heladospopsy.com>
2026-04-17 17:31:06 +0000

    File changes

--- src/components/menu/AppHeader.jsx
+++ src/components/menu/AppHeader.jsx
@@ -37,7 +37,7 @@ export default function AppHeader({ storeName, onLogoClick, onCartClick, onStore
             src={LOGO_URL}
             alt="Popsy"
             style={{
-              height: 186,
+              height: 124,
               width: "auto",
               objectFit: "contain",
               filter: "brightness(0) invert(1)",
